6560009a012e813a8b37b31b4dce7fee4e3c2a60
Rich <rich@posert.com>
2022-01-31 20:45:29 -0800

    Working

---
+++ ACAB.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(ggforce)
+library(extrafont)
+
+assault_data <- read_csv('all_state_data.csv', col_types = 'cddd')
+felony_data <- read_csv('felony-assault.csv', col_types = 'clc')
+
+data <- left_join(assault_data, felony_data, by = 'state') %>% 
+  select(-`source/notes`)
+
+data <- data %>% 
+  pivot_longer(cols = c('all_healthcare', 'hospitals', 'nursing_and_res_care'), names_to = 'Industry', values_to = 'Assaults')
+
+data %>% 
+  drop_na() %>% 
+  mutate(is_felony = if_else(is_felony, 'Felony', 'Misdemeanor')) %>% 
+  mutate(is_felony = fct_relevel(factor(is_felony), 'Misdemeanor', 'Felony')) %>% 
+  mutate(Industry = case_when(
+    Industry == 'all_healthcare' ~ 'All Healthcare',
+    Industry == 'hospitals' ~ 'Hospitals',
+    Industry == 'nursing_and_res_care' ~ 'Nursing and Residential Care'
+  )) %>% 
+  ggplot(aes(x = is_felony, y = Assaults)) +
+  theme_minimal() +
+  geom_boxplot(
+    color = 'grey',
+    outlier.shape = NA
+    ) +
+  geom_sina(maxwidth = 0.5) +
+  facet_grid(cols = vars(Industry)) +
+  labs(
+    title = 'Normalized assaults per 100 workers in three healthcare industries',
+    subtitle = 'Number of illnesses and injuries divided by the total hours worked,\nadjusted to the equivalent of 100 full-time employees',
+    y = 'Total Recordable Cases',
+    x = element_blank(),
+    caption = 'Sources:\nAssault case numbers are 2020 data, from the BLS: https://www.bls.gov/iif/oshstate.htm\nLaw data is from the actual text of laws on the books as of 2014, from onlabor.org, included in github repo\nRich Posert, https://github.com/PlethoraChutney/Healthcare-Assault-Datavis'
+  )
--- state_scraper.py
+++ state_scraper.py
@@ -5,15 +5,18 @@ import pandas as pd
 base_url = 'https://www.bls.gov'
 r = requests.get(base_url + '/iif/oshstate.htm')
 
-soup = BeautifulSoup(r.content)
+soup = BeautifulSoup(r.content, features = 'html.parser')
 
 table = soup.findChildren('table')[0]
 rows = table.findChildren('tr')
+
+states = []
 
 for row in rows:
     csv_link = None
     cells = row.findChildren('td')
     try:
+        state = cells[0].text.strip().replace('SOII', '').replace('/CFOI', '')
         cell = cells[2]
     except IndexError:
         continue
@@ -25,11 +28,36 @@ for row in rows:
             break
 
     if csv_link is not None:
-        print(base_url + csv_link)
         data = pd.read_excel(base_url + csv_link, header=1)
         data.columns = [x.rstrip() for x in data.columns]
-        print(data)
-        try:
-            print(data[data['NAICS Code3'] == 62])
-        except KeyError:
+        # NAICS Code has a 3 on it because of the footnote.
+        if 'NAICS Code3' not in data.columns:
             continue
+        try:
+            all_healthcare = data[data['NAICS Code3'] == '62']
+            all_healthcare = float(all_healthcare.iloc[0]['Total recordable cases'])
+        # have to except a ValueError too, all because Montana has a (-10-) in its data for some reason
+        except (IndexError, ValueError):
+            all_healthcare = None
+        try:
+            hospitals = data[data['NAICS Code3'] == '622']
+            hospitals = float(hospitals.iloc[0]['Total recordable cases'])
+        except (IndexError, ValueError):
+            hospitals = None
+        try:
+            nursing_and_res_care = data[data['NAICS Code3'] == '623']
+            nursing_and_res_care = float(nursing_and_res_care.iloc[0]['Total recordable cases'])
+        except (IndexError, ValueError):
+            nursing_and_res_care = None
+        state_dict = {
+            'state': state,
+            'all_healthcare': all_healthcare,
+            'hospitals': hospitals,
+            'nursing_and_res_care': nursing_and_res_care
+            }
+
+        if not all([x is None for x in state_dict.values()]):
+            states.append(state_dict)
+
+final_data = pd.DataFrame(states)
+final_data.to_csv('all_state_data.csv', index = False)
