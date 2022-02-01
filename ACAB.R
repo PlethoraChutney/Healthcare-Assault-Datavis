649bdd95603d20fa813e9aee80b4d208d32ade17
Rich <rich@posert.com>
2022-01-31 21:13:33 -0800

    Add annotations

--- ACAB.R
+++ ACAB.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggforce)
 library(extrafont)
+loadfonts(device = 'win')
 
 assault_data <- read_csv('all_state_data.csv', col_types = 'cddd')
 felony_data <- read_csv('felony-assault.csv', col_types = 'clc')
@@ -22,6 +23,7 @@ data %>%
   )) %>% 
   ggplot(aes(x = is_felony, y = Assaults)) +
   theme_minimal() +
+  theme(text = element_text(family = 'Lato')) +
   geom_boxplot(
     color = 'grey',
     outlier.shape = NA
@@ -29,9 +31,12 @@ data %>%
   geom_sina(maxwidth = 0.5) +
   facet_grid(cols = vars(Industry)) +
   labs(
-    title = 'Normalized assaults per 100 workers in three healthcare industries',
+    title = 'Assaults per 100 workers in three healthcare industries',
     subtitle = 'Number of illnesses and injuries divided by the total hours worked,\nadjusted to the equivalent of 100 full-time employees',
     y = 'Total Recordable Cases',
     x = element_blank(),
     caption = 'Sources:\nAssault case numbers are 2020 data, from the BLS: https://www.bls.gov/iif/oshstate.htm\nLaw data is from the actual text of laws on the books as of 2014, from onlabor.org, included in github repo\nRich Posert, https://github.com/PlethoraChutney/Healthcare-Assault-Datavis'
   )
+ggsave('assault_data.png', width = 8, height = 6)
+
+
